8bdc0bf47ef6a4ca605d87b736c553c0a5ea3693
unknown <kalyansingh277@gmail.com>
2020-09-12 17:29:15 +0530

    my first commit

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class TodoItem extends Component {
+    render() {
+        const { title, handleDelete, handleEdit } = this.props;
+        return (
+          <li
+            className="list-group-item text-capitalize d-flex justify-content-between my-2"
+            style={{ "backgroundColor": "aquamarine" }}
+          >
+            <h6>{title}</h6>
+            <div className="todo-icon">
+              <span className="mx-2 text-success">
+                <i className="fas fa-pen" onClick={handleEdit} />
+              </span>
+              <span className="mx-2 text-danger" onClick={handleDelete}>
+                <i className="fas fa-trash" />
+              </span>
+            </div>
+          </li>
+        );
+    }
+}
